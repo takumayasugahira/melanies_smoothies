3f995c41ed21ce133ead0fde9570f278266a731c
takumayasugahira <yasugahira.takuma.4909@members.co.jp>
2024-11-13 13:53:11 +0900

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,39 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothies!
+    """
+)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients'
+    ,my_dataframe
+    ,max_selections = 5
+)
+
+if ingredients_list:
+    ingredients_string = ' '.join(ingredients_list).strip()  # スペースで結合し、余分なスペースを削除
+
+    my_insert_stmt = f"""INSERT INTO smoothies.public.orders (ingredients, name_on_order)
+        VALUES ('{ingredients_string}', '{name_on_order}')"""
+    #st.write(my_insert_stmt) 
+
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        try:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
